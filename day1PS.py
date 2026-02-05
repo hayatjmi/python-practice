8b81b41c12503e70ec20930392fe63feeff432aa
Hayat Khan <hayatjmi007@gmail.com>
2026-02-05 20:51:39 +0530

    PS1

--- day1PS.py
+++ day1PS.py
@@ -1,79 +1,79 @@
 # Level 1
 # Question 1
-# channel_name = "code_logic_hub"
-# print("Welcom to "+ channel_name)
+channel_name = "code_logic_hub"
+print("Welcom to "+ channel_name)
 
-# # Question 2
-# name = input("Enter Your Name:")
-# print("Hello "+ name)
+# Question 2
+name = input("Enter Your Name:")
+print("Hello "+ name)
 
-# # Question 3
-# num1 = input("Enter first number: ")
-# num2 = input("Enter second number: ")
-# sum = int(num1)+int(num2)
-# sub = int(num1)-int(num2)
-# mul = int(num1)*int(num2)
-# print(sum)
-# print(sub)
-# print(mul)
+# Question 3
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+sum = int(num1)+int(num2)
+sub = int(num1)-int(num2)
+mul = int(num1)*int(num2)
+print(sum)
+print(sub)
+print(mul)
 
-# # Level 2
-# # Question 4
-# num = input("Enter the number: ")
-# if int(num)%2 == 0:
-#  print("Even")
+# Level 2
+# Question 4
+num = input("Enter the number: ")
+if int(num)%2 == 0:
+ print("Even")
 
-# else:
-#  print("Odd") 
+else:
+ print("Odd") 
 
-# #  Question 5
-# age = input("Enter your age: ")
-# if int(age)>18:
-#  print("Permission Granted!")
+#  Question 5
+age = input("Enter your age: ")
+if int(age)>18:
+ print("Permission Granted!")
 
-# else:
-#  print("Permission Denied !") 
+else:
+ print("Permission Denied !") 
 
 
-# # Question 6
-# for i in range(1,11):
-#  print(i)
+# Question 6
+for i in range(1,11):
+ print(i)
 
-# # Or
-# count =1
-# while count<=10:
-#  print(count) 
-#  count +=1
+# Or
+count =1
+while count<=10:
+ print(count) 
+ count +=1
 
 
 #  LEVEL 3
 #  Question 7
-# technologies = ["html","css","python"]
-# technologies.append("flask")
-# technologies.remove("css")
-# for item in technologies:
-#  print(item)
+technologies = ["html","css","python"]
+technologies.append("flask")
+technologies.remove("css")
+for item in technologies:
+ print(item)
 
 # Question 8
-# str = input("Enter Your Full Name: ")
-# print(str)
-# print(str.upper())
-# print(str.lower())
-# print(str.replace("k","n"))
-# print(len(str))
+str = input("Enter Your Full Name: ")
+print(str)
+print(str.upper())
+print(str.lower())
+print(str.replace("k","n"))
+print(len(str))
 
 # Question 9
-# correct_username = "admin"
-# correct_password = "12345"
+correct_username = "admin"
+correct_password = "12345"
 
-# username = input("Enter Username: ")
-# password = input("Enter Password: ")
+username = input("Enter Username: ")
+password = input("Enter Password: ")
 
-# if username == correct_username and password == correct_password:
-#     print("Credential Successful")
+if username == correct_username and password == correct_password:
+    print("Credential Successful")
 
-# else:
-#     print("Unsuccessful Credential")
+else:
+    print("Unsuccessful Credential")
 
 
 # Question 10 
